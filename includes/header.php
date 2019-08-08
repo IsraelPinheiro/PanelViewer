2eb99e2d1a8299a7e8c1ee171391986149014f35
Israel R Pinheiro <israel.pinheiro@live.com>
2019-08-08 14:48:23 -0300

    Corrigir imagem do header

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar fixed-top navbar-dark bg-dark">
         <a class="navbar-brand" href="/">
-            <img src="/img/Logo_30x30.png" width="auto" height="30" class="d-inline-block align-top" alt="">
+            <img src="/img/logo_30x30.png" width="auto" height="30" class="d-inline-block align-top" alt="">
             PanelViewer
         </a>
     </nav>
