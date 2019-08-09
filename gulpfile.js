6f220007eee3e8028926b2124c3cc31cb39dbda3
Israel R Pinheiro <israel.pinheiro@live.com>
2019-08-09 12:53:49 -0300

    Rename vendor folder to bin and correct references

--- gulpfile.js
+++ gulpfile.js
@@ -41,33 +41,33 @@ function browserSyncReload(done){
     done();
 }
 
-// Clean vendor
+// Clean bin
 function clean(){
-    return del(["./vendor/"]);
+    return del(["./bin/"]);
 }
 
-// Bring third party dependencies from node_modules into vendor directory
+// Bring third party dependencies from node_modules into bin directory
 function modules(){
     //Bootstrap JS
     var bootstrapJS = gulp.src('./node_modules/bootstrap/dist/js/*')
-        .pipe(gulp.dest('./vendor/bootstrap/js'));
+        .pipe(gulp.dest('./bin/bootstrap/js'));
     //Popper.js
     var popperJS = gulp.src('./node_modules/popper.js/dist/umd/*')
-        .pipe(gulp.dest('./vendor/popper.js'));
+        .pipe(gulp.dest('./bin/popper.js'));
     //jQuery Mask Plugin
     var jQueryMaskPlugin = gulp.src('./node_modules/jquery-mask-plugin/src/*')
-        .pipe(gulp.dest('./vendor/jquery-mask-plugin'));
+        .pipe(gulp.dest('./bin/jquery-mask-plugin'));
     //Font Awesome CSS
     var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
-        .pipe(gulp.dest('./vendor/fontawesome-free/css'));
+        .pipe(gulp.dest('./bin/fontawesome-free/css'));
     //Font Awesome Webfonts
     var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-        .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
+        .pipe(gulp.dest('./bin/fontawesome-free/webfonts'));
     //jQuery
     var jquery = gulp.src([
         './node_modules/jquery/dist/*',
         '!./node_modules/jquery/dist/core.js'
-    ]).pipe(gulp.dest('./vendor/jquery'));
+    ]).pipe(gulp.dest('./bin/jquery'));
     return merge(bootstrapJS, jQueryMaskPlugin, fontAwesomeCSS, fontAwesomeWebfonts,popperJS, jquery);
 }
 
@@ -123,15 +123,15 @@ function watchFiles() {
 }
 
 // Define complex tasks
-const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
+const bin = gulp.series(clean, modules);
+const build = gulp.series(bin, gulp.parallel(css, js));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
 exports.css = css;
 exports.js = js;
 exports.clean = clean;
-exports.vendor = vendor;
+exports.bin = bin;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
--- includes/head.php
+++ includes/head.php
@@ -2,13 +2,13 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>PanelViewer</title>
     <!--JQuery-->
-    <script src="vendor/jquery/jquery.slim.min.js"></script>
+    <script src="bin/jquery/jquery.slim.min.js"></script>
     <!--Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="vendor/bootstrap/bootstrap.min.js"></script>
+    <script src="bin/bootstrap/bootstrap.min.js"></script>
     <!--FontAwesome 5.7.2-->
-    <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="bin/fontawesome-free/css/all.min.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="js/app.js"></script>
     <link rel="stylesheet" href="css/app.css">
 </head>
